bcf396b8655df421bcef91a405a2adb271214575
EliasAHH <eliasiscoding@gmail.com>
2021-05-04 12:29:12 -0700

    mostly done just minor adjustments and need to add pictures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import {Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 
-function App() {
+import Home from "./containers/Home";
+import Creeper from "./containers/Creeper"
+import Profile from "./containers/Profile"
+import { fetchUser,getAllUsers } from './Redux/actioncreators/useractions'
+// import '../stylesheets/Home.scss';
+
+
+const App = (props) => {
+
+  useEffect(() =>{
+    if(localStorage.token) {
+      props.fetchUser(localStorage.token)
+    }
+     console.log("How many times does useEffect updates?")
+
+    console.log("inside of useEffect APP")
+  }, [])
+
+  console.log("before the return inside of the APP")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <Header />
+    <Switch>
+        <Route exact path="/creeper">
+          <Creeper />
+        </Route>
+        
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
 
-export default App;
+        <Route exact path="/">
+           <Home />
+        </Route>
+
+    </Switch>
+      {/* <Header />
+      {people[1] ? (
+        <Person
+          key={people[1].id}
+          person={people[1]}
+          modifySuperficialChoices={modifySuperficialChoices}
+          likedUsers={likedUsers}
+        />
+      ) : (
+        <Lonely
+          activeUserImage={people[activeUser].image}
+          likedUsers={likedUsers}
+          superLikedUsers={superLikedUsers}
+        />
+      )} */}
+    </>
+  );
+};
+
+export default connect(null, { fetchUser,getAllUsers })(App);
